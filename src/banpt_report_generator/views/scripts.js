d2a87c4fb3473c35112992cc4f642191cf9b7570
Jonathan Christopher <jonathan.christopher@outlook.com>
2018-03-13 22:50:41 +0700

    Change report tables tab appearance to be more user-friendly (dropdown instead of tabs)

---
+++ src/banpt_report_generator/views/scripts.js
@@ -0,0 +1,14 @@
+
+var banpt_applyJqueryFixesInterval;
+
+$(document).ready(function () {
+  banpt_collapseReportTablesTabsInterval = setInterval(function () {
+    // Clear all active classes on tab change as it won't be cleared automatically due to the HTML changes
+    var tabs = $('.banpt_report_tables_dropdown a[data-toggle="tab"]');
+    tabs.parent().removeClass('active');
+    tabs.off('shown.bs.tab');
+    tabs.on('shown.bs.tab', function (e) {
+      $(e.target).parent().removeClass('active');
+    });
+  }, 5000);
+});
