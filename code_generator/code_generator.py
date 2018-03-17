713c3c458c93fa010d314b119044c8f80ba55cba
Jonathan Christopher <jonathan.christopher@outlook.com>
2018-03-17 10:09:59 +0700

    Add report view generator

--- code_generator/code_generator.py
+++ code_generator/code_generator.py
@@ -5,9 +5,11 @@ import re
 import pprint
 import string
 from model_view_generator import generate_model_view
+from report_view_generator import generate_report_view
 
 ignore_model_files = ['__init__.py', 'report.py']
 model_directory = '../src/banpt_report_generator/models'
+generated_directory = './generated'
 generated_model_views_directory = './generated/model_views'
 
 if __name__ == "__main__":
@@ -18,24 +20,32 @@ if __name__ == "__main__":
 
     model_files = [f[:-3] for f in listdir(model_directory) if path.isfile(path.join(model_directory, f)) and f not in ignore_model_files and f[-3:] == '.py']
     print 'Generating views for models:'
-    pp.pprint(model_files)
 
     # Read and parse model fields, then generate model views
 
+    title_pattern = re.compile("^\s*_title = ["'](.*)['"]\s*$")
     field_pattern = re.compile("^\s*([a-zA-Z0-9_]+) = fields\.([a-zA-Z0-9_]+)\((.*)\)\s*$")
 
+    models = []
     for model_file in model_files:
         model_path = path.join(model_directory, model_file + '.py')
         model_fields = []
+        model_title = string.capwords(model_file)
 
         for i, line in enumerate(open(model_path)):
+            for match in re.finditer(title_pattern, line):
+                model_title = match.groups()[0]
             for match in re.finditer(field_pattern, line):
                 model_fields.append(match.groups())
+
+        models.append(dict(name=model_file, title=model_title))
+        print '- ' + model_file + ': ' + model_title
         
         generate_model_view(
             name=model_file,
-            title=string.capwords(model_file),
+            title=model_title,
             fields=[f[0] for f in model_fields],
             directory=generated_model_views_directory
         )
 
+    generate_report_view(models=models, directory=generated_directory)
---
+++ code_generator/report_view_generator.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+
+import string
+from os import path
+
+view_template = string.Template(
+"""<?xml version="1.0" encoding="UTF-8"?>
+<odoo>
+    <data>
+        <record model="ir.ui.view" id="view_report_tree">
+            <field name="name">Laporan</field>
+            <field name="model">banpt_report_generator.report</field>
+            <field name="type">tree</field>
+            <field name="arch" type="xml">
+                <tree>
+                    <field name="name" />
+                    <field name="year" />
+                    <field name="prodi" />
+                </tree>
+            </field>
+        </record>
+        <record model="ir.ui.view" id="view_report_form">
+            <field name="name">Laporan</field>            
+            <field name="model">banpt_report_generator.report</field>
+            <field name="type">form</field>
+            <field name="priority" eval="0" />
+            <field name="arch" type="xml">
+                <form string="Laporan">
+                    <header>
+                        <button string="Setujui" type="object" name="approve" attrs="{'invisible': [('state', '=', 'approved')]}" class="oe_highlight" />
+                        <field name="state" widget="statusbar" />
+                    </header>
+                    <group>
+                        <field name="id" invisible="1" />
+                        <field name="name" />
+                        <field name="year" attrs="{'readonly': [('id', '!=', False)]}" />
+                        <field name="prodi" attrs="{'readonly': [('id', '!=', False)]}" />
+                        <field name="description" />
+                    </group>
+                    <hr/>
+                    <div class="banpt_notebook_group banpt_report_tables">
+                        <div class="dropdown" id="banpt_report_tables_menu_container">
+                            <a class="btn dropdown-toggle banpt_report_tables_menu" data-toggle="dropdown" href="#">
+                                <span class="banpt_report_tables_menu_text">Pilih tabel </span><span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu banpt_report_tables_dropdown" role="tablist">
+                                <!-- Add table report dropdown menu items here -->
+${dropdown_menu_items}
+                            </ul>
+                        </div>
+                        <div class="tab-content">
+                            <!-- Add table report contents here -->
+${contents}
+                        </div>
+                    </div>
+                </form>
+            </field>
+        </record>
+
+        <record model="ir.actions.act_window" id="view_report_action">
+            <field name="name">Laporan</field>
+            <field name="res_model">banpt_report_generator.report</field>
+            <field name="view_mode">tree,form</field>
+            <field name="view_type">form</field>
+            <field name="help" type="html">
+                <p class="oe_view_nocontent_create">Belum ada laporan yang telah dibuat</p>
+            </field>
+        </record>
+
+        <record model="ir.actions.act_window.view" id="view_report_tree_action">
+            <field name="sequence" eval="0" />
+            <field name="view_mode">tree</field>
+            <field name="view_id" ref="view_report_tree" />
+            <field name="act_window_id" ref="view_report_action" />
+        </record>
+
+        <record model="ir.actions.act_window.view" id="view_report_form_action">
+            <field name="sequence" eval="1" />
+            <field name="view_mode">form</field>
+            <field name="view_id" ref="view_report_form" />
+            <field name="act_window_id" ref="view_report_action" />
+        </record>
+    </data>
+</odoo>
+""")
+
+dropdown_menu_item_template = string.Template(
+"""                                <li role="presentation">
+                                    <a href="#tab_${model_name}" aria-controls="tab_${model_name}" role="tab" data-toggle="tab">${model_title}</a>
+                                </li>"""
+)
+
+content_template = string.Template(
+"""                            <div role="tabpanel" class="tab-pane active" id="tab_${model_name}">
+                                <h3 class="banpt_notebook_page_title">${model_title}</h3>
+                                <field name="${model_name}" />
+                            </div>"""
+)
+
+def generate_report_view(models, directory):
+    dropdown_menu_items = []
+    contents = []
+    for model in models:
+        model_params = dict(model_name=model['name'], model_title=model['title'])
+        dropdown_menu_items.append(dropdown_menu_item_template.substitute(model_params))
+        contents.append(content_template.substitute(model_params))
+
+    view_template_params = dict(
+        dropdown_menu_items=string.join(dropdown_menu_items, '\n'),
+        contents=string.join(contents, '\n')
+    )
+    generated_view = view_template.substitute(view_template_params)
+
+    view_path = path.join(directory, 'report_views.xml')
+    with open(view_path, 'w') as fout:
+        fout.write(generated_view)
