8463574da818959b4df4015a3fa4c33d5144c379
Jonathan Christopher <jonathan.christopher@outlook.com>
2018-03-17 10:17:46 +0700

    Add model access generator

--- code_generator/code_generator.py
+++ code_generator/code_generator.py
@@ -6,6 +6,7 @@ import pprint
 import string
 from model_view_generator import generate_model_view
 from report_view_generator import generate_report_view
+from model_access_generator import generate_model_access
 
 ignore_model_files = ['__init__.py', 'report.py']
 model_directory = '../src/banpt_report_generator/models'
@@ -49,3 +50,4 @@ if __name__ == "__main__":
         )
 
     generate_report_view(models=models, directory=generated_directory)
+    generate_model_access(models=models, directory=generated_directory)
