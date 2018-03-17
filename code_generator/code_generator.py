3fd155012c11d8a3700b8537551b51bfea2fcc67
Jonathan Christopher <jonathan.christopher@outlook.com>
2018-03-17 09:33:22 +0700

    Add model view generator

--- code_generator/code_generator.py
+++ code_generator/code_generator.py
@@ -3,29 +3,39 @@
 from os import listdir, path
 import re
 import pprint
+import string
+from model_view_generator import generate_model_view
 
 ignore_model_files = ['__init__.py', 'report.py']
 model_directory = '../src/banpt_report_generator/models'
+generated_model_views_directory = './generated/model_views'
 
 if __name__ == "__main__":
 
     pp = pprint.PrettyPrinter(indent=2)
 
+    # List model files
+
     model_files = [f[:-3] for f in listdir(model_directory) if path.isfile(path.join(model_directory, f)) and f not in ignore_model_files and f[-3:] == '.py']
     print 'Generating views for models:'
     pp.pprint(model_files)
 
+    # Read and parse model fields, then generate model views
+
     field_pattern = re.compile("^\s*([a-zA-Z0-9_]+) = fields\.([a-zA-Z0-9_]+)\((.*)\)\s*$")
-    model_fields = {}
 
     for model_file in model_files:
         model_path = path.join(model_directory, model_file + '.py')
-        model_fields[model_file] = {}
-        model_fields[model_file] = []
+        model_fields = []
+
         for i, line in enumerate(open(model_path)):
             for match in re.finditer(field_pattern, line):
-                model_fields[model_file].append(match.groups())
+                model_fields.append(match.groups())
+        
+        generate_model_view(
+            name=model_file,
+            title=string.capwords(model_file),
+            fields=[f[0] for f in model_fields],
+            directory=generated_model_views_directory
+        )
 
-    print 'Model fields:'
-    pp.pprint(model_fields)
-                
---
+++ code_generator/model_view_generator.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+import string
+from os import path
+
+view_template = string.Template(
+"""<?xml version="1.0" encoding="UTF-8"?>
+<odoo>
+    <data>
+        <record model="ir.ui.view" id="view_${name}_tree">
+            <field name="name">${title}</field>
+            <field name="model">banpt_report_generator.${name}</field>
+            <field name="type">tree</field>
+            <field name="arch" type="xml">
+                <tree editable="bottom">
+${tree_fields}
+                </tree>
+            </field>
+        </record>
+
+        <record model="ir.ui.view" id="view_${name}_form">
+            <field name="name">${title}</field>
+            <field name="model">banpt_report_generator.${name}</field>
+            <field name="type">form</field>
+            <field name="arch" type="xml">
+                <form string="${title}">
+                    <group>
+${form_fields}
+                    </group>
+                </form>
+            </field>
+        </record>
+
+         <record model="ir.actions.act_window" id="view_${name}_action">
+            <field name="name">${title}</field>
+            <field name="res_model">banpt_report_generator.${name}</field>
+            <field name="view_mode">tree,form</field>
+            <field name="view_type">form</field>
+        </record>
+
+        <record model="ir.actions.act_window.view" id="view_${name}_tree_action">
+            <field name="sequence" eval="0" />
+            <field name="view_mode">tree</field>
+            <field name="view_id" ref="view_${name}_tree" />
+            <field name="act_window_id" ref="view_${name}_action" />
+        </record>
+
+        <record model="ir.actions.act_window.view" id="view_${name}_form_action">
+            <field name="sequence" eval="1" />
+            <field name="view_mode">form</field>
+            <field name="view_id" ref="view_${name}_form" />
+            <field name="act_window_id" ref="view_${name}_action" />
+        </record>
+    </data>
+</odoo>
+""")
+
+tree_field_template = string.Template('                    <field name="$field_name" />')
+form_field_template = string.Template('                        <field name="$field_name" />')
+
+def generate_model_view(name, title, fields, directory):
+    tree_fields = []
+    form_fields = []
+    for field in fields:
+        tree_fields.append(tree_field_template.substitute(dict(field_name=field)))
+        form_fields.append(form_field_template.substitute(dict(field_name=field)))
+
+    view_template_params = dict(
+        name=name,
+        title=title,
+        tree_fields=string.join(tree_fields, '\n'),
+        form_fields=string.join(form_fields, '\n')
+    )
+    generated_view = view_template.substitute(view_template_params)
+
+    view_path = path.join(directory, name + '_views.xml')
+    with open(view_path, 'w') as fout:
+        fout.write(generated_view)
