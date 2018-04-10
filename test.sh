065fc9427cb0fb5a0f508f4ffedfed1654ef5891
Jonathan Christopher <jonathan.christopher@outlook.com>
2018-04-10 12:13:29 +0700

    Fix test script not failing errors

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker-compose -f docker-compose.test.yml up --abort-on-container-exit | grep FAIL; test $? -eq 1
+docker-compose -f docker-compose.test.yml up --abort-on-container-exit | grep -E "(ERROR|FAIL)"; test $? -eq 1
