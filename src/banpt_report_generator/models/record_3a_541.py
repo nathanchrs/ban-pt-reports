e0bec539be426c0dd473fd3abcba758edd675d1e
adrianhp97 <adrianhp100797@gmail.com>
2018-04-15 11:39:54 +0700

    initialize function refresh in record 3a_541

--- src/banpt_report_generator/models/record_3a_541.py
+++ src/banpt_report_generator/models/record_3a_541.py
@@ -16,4 +16,8 @@ class Record_3A_541(models.Model):
     report_refresh_date = fields.Datetime(related='report.refresh_date')
 
 def refresh(reports):
-    pass
+    for report in reports:
+        # Clear record_3a_541 table
+        report.record_3a_541.unlink()
+
+        # add record_3a_541 according to program_id
