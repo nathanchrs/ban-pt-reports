280d18e72d84a1166bc130fccc71afebfba3bae9
Faiz G. Haznitrama <faizghifari@students.itb.ac.id>
2018-04-16 13:05:04 +0700

    initialize refresh function 3a_451-3a_453

--- src/banpt_report_generator/models/record_3a_451.py
+++ src/banpt_report_generator/models/record_3a_451.py
@@ -17,4 +17,8 @@ class Record_3A_451(models.Model):
     report_refresh_date = fields.Datetime(related='report.refresh_date')
 
 def refresh(reports):
-    pass
+    for report in reports:
+        # Clear Record_3A_451
+        report.record_3a_451.unlink()
+
+        # Add kegiatan pakar
--- src/banpt_report_generator/models/record_3a_453.py
+++ src/banpt_report_generator/models/record_3a_453.py
@@ -19,4 +19,8 @@ class Record_3A_453(models.Model):
     report_refresh_date = fields.Datetime(related='report.refresh_date')
 
 def refresh(reports):
-    pass
+    for report in reports:
+        # Clear Record_3A_453
+        report.record_3a_453.unlink()
+
+        # Add kegiatan dosen tetap
