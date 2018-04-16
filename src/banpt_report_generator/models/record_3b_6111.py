0572d966bd77f73a51fd0a8f5e384a2979da3d43
adrianhp97 <adrianhp100797@gmail.com>
2018-04-16 23:35:00 +0700

    add model 3b_6111

---
+++ src/banpt_report_generator/models/record_3b_6111.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Record_3B_6111(models.Model):
+    _name = 'banpt_report_generator.record_3b_6111'
+    _rec_name = 'jenis_dana'
+    _title = '3B-6.1.1.1 Jumlah Dana Yang Diterima Fakultas'
+
+    sumber_dana = fields.Text(string='Sumber Dana', required=True)
+    jenis_dana = fields.Text(string='Jenis Dana', required=True)
+    jumlah_dana_ts_2 = fields.Integer(string='Jumlah Dana TS-2 (juta rupiah)')
+    jumlah_dana_ts_1 = fields.Integer(string='Jumlah Dana TS-1 (juta rupiah)')
+    jumlah_dana_ts = fields.Integer(string='Jumlah Dana TS (juta rupiah)')
+
+    # The report this record belongs to
+    report = fields.Many2one(comodel_name='banpt_report_generator.report')
+    report_refresh_date = fields.Datetime(related='report.refresh_date')
+
+def refresh(reports):
+    pass
