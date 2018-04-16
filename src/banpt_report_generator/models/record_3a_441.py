a12b657ac923d90ee3528c64049840350eae1a04
Faiz G. Haznitrama <faizghifari@students.itb.ac.id>
2018-04-16 13:00:03 +0700

    initialize refresh function 3a_441

--- src/banpt_report_generator/models/record_3a_441.py
+++ src/banpt_report_generator/models/record_3a_441.py
@@ -27,4 +27,8 @@ class Record_3A_441(models.Model):
     report_refresh_date = fields.Datetime(related='report.refresh_date')
 
 def refresh(reports):
-    pass
+    for report in reports:
+        # Clear Record_3A_441 table
+        report.record_3a_441.unlink()
+
+        # Add dosen tidak tetap
