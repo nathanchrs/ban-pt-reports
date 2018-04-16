decf6420feb94e358a9ecb2ed66b8f4817957b4d
Faiz G. Haznitrama <faizghifari@students.itb.ac.id>
2018-04-16 12:57:44 +0700

    initialize refresh function 3a_435

--- src/banpt_report_generator/models/record_3a_435.py
+++ src/banpt_report_generator/models/record_3a_435.py
@@ -19,4 +19,8 @@ class Record_3A_435(models.Model):
     report_refresh_date = fields.Datetime(related='report.refresh_date')
 
 def refresh(reports):
-    pass
+    for report in reports:
+        # Clear Record_3A_435 tables
+        report.record_3a_435.unlink()
+
+        # Add aktivitas mengajar dosen tetap
