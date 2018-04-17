309e4bc0cead34b030f9d6582d1773fdac003cd0
Faiz G. Haznitrama <faizghifari@students.itb.ac.id>
2018-04-17 08:00:54 +0700

    initialize refresh function 3a_714

--- src/banpt_report_generator/models/record_3a_714.py
+++ src/banpt_report_generator/models/record_3a_714.py
@@ -14,4 +14,8 @@ class Record_3A_714(models.Model):
     report_refresh_date = fields.Datetime(related='report.refresh_date')
 
 def refresh(reports):
-    pass
+    for report in reports:
+        # Clear Record_3A_714 table
+        report.record_3a_714.unlink()
+
+        # Add HAKI table
