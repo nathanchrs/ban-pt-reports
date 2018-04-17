6256f7b22df63303872edfa6270abe269b68ca6c
Faiz G. Haznitrama <faizghifari@students.itb.ac.id>
2018-04-17 07:59:29 +0700

    initialize refresh function 3a_713

--- src/banpt_report_generator/models/record_3a_713.py
+++ src/banpt_report_generator/models/record_3a_713.py
@@ -20,4 +20,8 @@ class Record_3A_713(models.Model):
     report_refresh_date = fields.Datetime(related='report.refresh_date')
 
 def refresh(reports):
-    pass
+    for report in reports:
+        # Clear Record_3A_713 table
+        report.record_3a_713.unlink()
+
+        # Add judul artikel ilmiah or others table
