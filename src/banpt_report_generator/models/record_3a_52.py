af1ee295c0502d82ae0341d8877d8f3277b9be31
Adya Naufal Fikri <13515130@std.stei.itb.ac.id>
2018-04-19 07:30:25 +0700

    add table 3a_52 model

---
+++ src/banpt_report_generator/models/record_3a_52.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Record_3A_52(models.Model):
+    _name = 'banpt_report_generator.record_3a_52'
+    _rec_name = 'not_found'
+    _title = '3A-5.2 Not Found'
+
+    not_found = fields.Char(string='Not Found', required=True)
+    
+    # The report this record belongs to
+    report = fields.Many2one(comodel_name='banpt_report_generator.report')
+    report_refresh_date = fields.Datetime(related='report.refresh_date')
+
+def refresh(reports):
+    pass
