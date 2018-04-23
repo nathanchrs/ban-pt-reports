10333cd643295f234ba22b7d7fd61cbe7d93a9e7
adrianhp97 <adrianhp100797@gmail.com>
2018-04-23 18:38:21 +0700

    edit test for table 3a_5121

--- src/banpt_report_generator/tests/test_record_3a_5121.py
+++ src/banpt_report_generator/tests/test_record_3a_5121.py
@@ -6,64 +6,46 @@ def seed(context):
     prodi_if = context.env['itb.academic_program'].create(dict(
         name='Teknik Informatika', name_en='Informatics', prefix='135', degree='s1', active=True
     ))
+    prodi_el = context.env['itb.academic_program'].create(dict(
+        name='Teknik Elektro', name_en='Electrical Engineering', prefix='182', degree='s1', active=True
+    ))
 
     catalog_1 = context.env['itb.academic_catalog'].create(dict(
-        name='Catalog 1',
-        code='code1',
-        credit=4
+        name='Katalog 1',
+        code='k01',
+        credit=2,
+        note='This is note',
+        syllabus='This is syllabus'
     ))
-
-    catalog_2 = context.env['itb.academic_catalog'].create(dict(
-        name='Catalog 2',
-        code='code2',
-        credit=3
-    ))
-    
-    catalog_3 = context.env['itb.academic_catalog'].create(dict(
-        name='Catalog 3',
-        code='code3',
-        credit=2
-    ))
-
-    catalog_4 = context.env['itb.academic_catalog'].create(dict(
-        name='Catalog 4',
-        code='code4',
-        credit=1
-    ))
-
     curriculum_1 = context.env['itb.academic_curriculum'].create(dict(
         name='Curriculum 1',
-        year=2015,
         program_id=prodi_if.id,
-        curriculum_line_ids=True
+        year=2015
     ))
-
     context.env['itb.academic_curriculum_line'].create(dict(
         curriculum_id=curriculum_1.id,
         catalog_id=catalog_1.id,
-        category='wajib',
-        semester=1
+        semester=0,
+        category='wajib'
     ))
 
+    catalog_2 = context.env['itb.academic_catalog'].create(dict(
+        name='Katalog 2',
+        code='k02',
+        credit=3,
+        note='This is note',
+        syllabus='This is syllabus'
+    ))
+    curriculum_2 = context.env['itb.academic_curriculum'].create(dict(
+        name='Curriculum 2',
+        program_id=prodi_el.id,
+        year=2016
+    ))
     context.env['itb.academic_curriculum_line'].create(dict(
-        curriculum_id=curriculum_1.id,
+        curriculum_id=curriculum_2.id,
         catalog_id=catalog_2.id,
-        category='opsional',
-        semester=1
-    ))
-
-    context.env['itb.academic_curriculum_line'].create(dict(
-        curriculum_id=curriculum_1.id,
-        catalog_id=catalog_3.id,
-        category='opsional-luar',
-        semester=1
-    ))
-
-    context.env['itb.academic_curriculum_line'].create(dict(
-        curriculum_id=curriculum_1.id,
-        catalog_id=catalog_4.id,
-        category='opsional-external',
-        semester=1
+        semester=0,
+        category='opsional'
     ))
 
     report_1 = context.env['banpt_report_generator.report'].create(dict(
@@ -73,20 +55,38 @@ def seed(context):
         prodi=prodi_if.id
     ))
 
-    return report_1
+    report_2 = context.env['banpt_report_generator.report'].create(dict(
+        name='Sample Report 2',
+        year=2016,
+        refresh_date='2016-07-05 08:00:00',
+        prodi=prodi_el.id
+    ))
+
+    return report_1, report_2
 
 class TestRecord_3A_5121(common.TransactionCase):
     def test_refresh(self):
-        report_1 = seed(self)
+        report_1, report_2 = seed(self)
 
         report_1.refresh()
+        report_2.refresh()
 
-        ts_1 = report_1.record_3a_5121.search([['jenis_mata_kuliah', '=', 'Wajib']])
-        self.assertEqual(ts_1.jenis_mata_kuliah, 'Wajib')
-        self.assertEqual(ts_1.sks, 4)
-        self.assertEqual(ts_1.keterangan, '')
+        catalog_1 = report_1.record_3a_5121.search([['jenis_mata_kuliah', '=', 'Wajib']])
+        self.assertEqual(catalog_1[0].jenis_mata_kuliah, 'Wajib')
+        self.assertEqual(catalog_1[0].sks, 2)
+        self.assertEqual(catalog_1[0].keterangan, '')
 
-        ts_2 = report_1.dosen.search([['jenis_mata_kuliah', '=', 'Pilihan']])
-        self.assertEqual(ts_2.jenis_mata_kuliah, 'Pilihan')
-        self.assertEqual(ts_2.sks, 6)
-        self.assertEqual(ts_2.keterangan, '')
+        catalog_11 = report_1.record_3a_5121.search([['jenis_mata_kuliah', '=', 'Pilihan']])
+        self.assertEqual(catalog_11[0].jenis_mata_kuliah, 'Pilihan')
+        self.assertEqual(catalog_11[0].sks, 0)
+        self.assertEqual(catalog_11[0].keterangan, '')
+
+        catalog_2 = report_2.record_3a_5121.search([['jenis_mata_kuliah', '=', 'Pilihan']])
+        self.assertEqual(catalog_2[1].jenis_mata_kuliah, 'Pilihan')
+        self.assertEqual(catalog_2[1].sks, 3)
+        self.assertEqual(catalog_2[1].keterangan, '')
+
+        catalog_22 = report_2.record_3a_5121.search([['jenis_mata_kuliah', '=', 'Wajib']])
+        self.assertEqual(catalog_22[1].jenis_mata_kuliah, 'Wajib')
+        self.assertEqual(catalog_22[1].sks, 0)
+        self.assertEqual(catalog_22[1].keterangan, '')
