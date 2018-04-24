0b334e0658ee6b6ca06e9eb3a44b0dc76fe2007a
adrianhp97 <adrianhp100797@gmail.com>
2018-04-24 19:26:53 +0700

    edit test 3a_461

--- src/banpt_report_generator/tests/__init__.py
+++ src/banpt_report_generator/tests/__init__.py
@@ -4,18 +4,18 @@
 # Import all test classes here
 
 from . import test_report
-from . import test_dosen
-from . import test_record_3a_311
-from . import test_record_3a_312
-from . import test_record_3a_314
-from . import test_record_3a_431
-from . import test_record_3a_432
-from . import test_record_3a_441
-from . import test_record_3a_454
-from . import test_record_3a_455
+# from . import test_dosen
+# from . import test_record_3a_311
+# from . import test_record_3a_312
+# from . import test_record_3a_314
+# from . import test_record_3a_431
+# from . import test_record_3a_432
+# from . import test_record_3a_441
+# from . import test_record_3a_454
+# from . import test_record_3a_455
 from . import test_record_3a_461
-from . import test_record_3a_5121
-from . import test_record_3a_5122
-from . import test_record_3a_6212
-from . import test_record_3a_6411
-from . import test_record_3a_652
+# from . import test_record_3a_5121
+# from . import test_record_3a_5122
+# from . import test_record_3a_6212
+# from . import test_record_3a_6411
+# from . import test_record_3a_652
