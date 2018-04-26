a2d72fc87ad32c0d4e61a206e7002a02a5d03ab3
Kukuh Basuki Rahmat <kukuhis777@gmail.com>
2018-04-26 15:39:33 +0700

    3a_711

--- src/banpt_report_generator/models/record_3a_711.py
+++ src/banpt_report_generator/models/record_3a_711.py
@@ -17,4 +17,56 @@ class Record_3A_711(models.Model):
     report_refresh_date = fields.Datetime(related='report.refresh_date')
 
 def refresh(reports):
-    pass
+    for report in reports:
+        # Clear record_3a_711 table
+        report.record_3a_711.unlink()
+
+        #add record_3a_711
+        new_record_3a_711 = {
+            'sumber_biaya': 'Pembiayaan sendiri oleh peneliti',
+            'ts_2': 0, #TODO
+            'ts_1': 0, #TODO
+            'ts': 0, #TODO
+        }
+
+        report.write({'record_3a_711': [(0, 0, new_record_3a_711)]})
+
+        #add record_3a_711
+        new_record_3a_711 = {
+            'sumber_biaya': 'PT yang bersangkutan',
+            'ts_2': 5, #TODO
+            'ts_1': 5, #TODO
+            'ts': 3, #TODO
+        }
+
+        report.write({'record_3a_711': [(0, 0, new_record_3a_711)]})
+
+        #add record_3a_711
+        new_record_3a_711 = {
+            'sumber_biaya': 'Depdiknas',
+            'ts_2': 12, #TODO
+            'ts_1': 8, #TODO
+            'ts': 17, #TODO
+        }
+
+        report.write({'record_3a_711': [(0, 0, new_record_3a_711)]})
+
+        #add record_3a_711
+        new_record_3a_711 = {
+            'sumber_biaya': 'Institusi dalam negeri di luar Depdiknas',
+            'ts_2': 0, #TODO
+            'ts_1': 1, #TODO
+            'ts': 1, #TODO
+        }
+
+        report.write({'record_3a_711': [(0, 0, new_record_3a_711)]})
+
+        #add record_3a_711
+        new_record_3a_711 = {
+            'sumber_biaya': 'Institusi luar negeri',
+            'ts_2': 0, #TODO
+            'ts_1': 0, #TODO
+            'ts': 0, #TODO
+        }
+
+        report.write({'record_3a_711': [(0, 0, new_record_3a_711)]})
